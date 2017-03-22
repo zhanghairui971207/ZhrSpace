5a2a00d53657dda1c41bf466bdf38c13cdacc6fe
unknown <>
2017-03-22 19:49:45 +0800

    1

---
+++ app/src/main/java/com/pc/zhrspace/MyApplication.java
@@ -0,0 +1,26 @@
+package com.pc.zhrspace;
+
+import android.app.Application;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+
+
+/**
+ * Created by pc on 2017/3/22.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EMOptions options = new EMOptions();
+// 默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
+
+//初始化
+        EMClient.getInstance().init(this, options);
+//在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(true);
+    }
+}
